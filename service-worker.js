72debaa4655779a5390c1ea7ef591b036e66bf76
BunmahonCGU <bunmahoncgu@gmail.com>
2026-04-18 21:48:48 +0100

    Add files via upload
    Initial files

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "bunmahon-cgu-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const url = new URL(event.request.url);
+
+  if (ASSETS.some(a => url.pathname.endsWith(a.replace("./", "")))) {
+    event.respondWith(
+      caches.match(event.request).then(resp => resp || fetch(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
