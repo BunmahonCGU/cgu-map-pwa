99901b872cc8ca88be3753ce158f245fb3517667
BunmahonCGU <bunmahoncgu@gmail.com>
2026-04-25 16:58:10 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,8 +1,32 @@
+const iconBase = "https://umap.openstreetmap.fr/static/img/markers/";
+
+const iconMap = {
+    // Cliff Walks
+    "CWA": iconBase + "stile.png",          // white stile icon
+    "CAP": iconBase + "circle-white.png",   // white circle
+
+    // Lake Access Points
+    "LR": iconBase + "circle-orange.png",   // orange line / access
+    "LA": iconBase + "circle-orange.png",   // orange circle with L#
+
+    // Defibrillators
+    "D": iconBase + "defibrillator.png",    // red heartbeat
+
+    // West Roads / Access Points
+    // WR = blue polyline (handled in style, no icon)
+    "WAP": iconBase + "circle-blue.png",    // blue circle with W#
+    "WJ": iconBase + "square-blue.png",     // blue square
+
+    // East Roads / Access Points
+    // ER = pink polyline (handled in style, no icon)
+    "EAP": iconBase + "circle-pink.png",    // pink circle with E#
+    "EJ": iconBase + "square-pink.png"      // pink square
+};
+
 async function loadUmapFile(url) {
     const response = await fetch(url);
     const umap = await response.json();
 
-    // Flatten all features from all layers
     const allFeatures = umap.layers.flatMap(layer => layer.features);
 
     return {
@@ -12,33 +36,52 @@ async function loadUmapFile(url) {
 }
 
 async function initMap() {
-    // Create the map
     const map = L.map('map').setView(
-        [52.10309691240007, -7.349853515625001], 
+        [52.10309691240007, -7.349853515625001],
         10
     );
 
-    // Base layer (OSM France)
     L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         maxZoom: 20,
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
-    // Load the uMap JSON file
     const geojson = await loadUmapFile('data/bunmahon-latest.umap');
 
-    // Render features
     L.geoJSON(geojson, {
-        pointToLayer: function (feature, latlng) {
-            const opts = feature.properties._umap_options || {};
-           // Always use a standard marker
-const icon = L.icon({
-    iconUrl: "https://unpkg.com/leaflet/dist/images/marker-icon.png",
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34]
-});
+        style: function (feature) {
+            const name = feature.properties.name || "";
+            const prefix = name.replace(/[0-9]/g, "");
 
+            if (feature.geometry.type === "LineString") {
+                if (prefix === "WR") {
+                    return { color: "blue", weight: 4 };
+                }
+                if (prefix === "ER") {
+                    return { color: "pink", weight: 4 };
+                }
+                if (prefix === "LR") {
+                    return { color: "orange", weight: 4 };
+                }
+            }
+
+            return {};
+        },
+
+        pointToLayer: function (feature, latlng) {
+            if (feature.geometry.type !== "Point") return;
+
+            const name = feature.properties.name || "";
+            const prefix = name.replace(/[0-9]/g, "");
+
+            const iconUrl = iconMap[prefix] || iconBase + "circle-blue.png";
+
+            const icon = L.icon({
+                iconUrl: iconUrl,
+                iconSize: [32, 32],
+                iconAnchor: [16, 32],
+                popupAnchor: [0, -32]
+            });
 
             return L.marker(latlng, { icon: icon });
         },
@@ -47,7 +90,6 @@ const icon = L.icon({
             if (feature.properties && feature.properties.description) {
                 let popup = feature.properties.description;
 
-                // Replace placeholders {lat} {lng} {lon}
                 if (feature.geometry && feature.geometry.coordinates) {
                     const [lon, lat] = feature.geometry.coordinates;
                     popup = popup
