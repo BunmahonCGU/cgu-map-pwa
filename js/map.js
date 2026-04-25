1e5fa44656017a904ec15649656e8416979262a9
BunmahonCGU <bunmahoncgu@gmail.com>
2026-04-26 00:03:07 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -34,11 +34,12 @@ function formatUmapPopup(raw) {
     txt.innerHTML = out;
     out = txt.value;
 
-   // 2) Convert uMap {{image}} syntax into <img> tags
+   // 2) Convert uMap {{image}} syntax into <img> tags (robust)
 out = out.replace(
-    /\{\{(https?:\/\/[^\}]+)\}\}/gi,
+    /\{\{(https?:\/\/.*?)\}\}/gi,
     '<img src="$1" style="max-width:100%; margin-top:6px;"/>'
 );
+
 
     // 3) Remove Markdown image syntax ![alt](url)
     out = out.replace(/!\[[^\]]*\]\([^)]+\)/g, "");
