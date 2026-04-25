fe0cacedac62474d22d76b21cd4107a982feaaad
BunmahonCGU <bunmahoncgu@gmail.com>
2026-04-25 18:08:18 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-const iconBase = "const iconBase = "https://umap.openstreetmap.fr/static/img/umap/markers/";
+const iconBase = "https://umap.openstreetmap.fr/static/img/umap/markers/";
 
 const iconMap = {
     // Cliff Walks
