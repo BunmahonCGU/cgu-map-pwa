1f306fa4e953b67f48cc6a61329c31fdce1c7dae
BunmahonCGU <bunmahoncgu@gmail.com>
2026-04-28 16:49:18 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -365,7 +365,7 @@ for (const [displayName, codes] of Object.entries(layerDisplayNames)) {
     overlays[displayName] = group;
 }
 
-L.control.layers(null, overlays, { collapsed: true }).addTo(map);
+L.control.layers(null, overlays, { collapsed: true });//.addTo(map);
 
     // Add all overlay groups to the map initially
 Object.values(overlays).forEach(group => group.addTo(map));
